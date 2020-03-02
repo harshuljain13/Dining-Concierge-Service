9d3aceb7e73e6a93f276e44e842509fdd0c7cdd6
harshul1610 <harshulrobo@gmail.com>
2020-03-02 18:19:14 -0500

    manhattan dataset

--- backend/yelp.py
+++ backend/yelp.py
@@ -6,10 +6,11 @@ def main():
     api_key = 'ucTJwlkUtHfkwSxf0JU93ukhwc8AC0fj8eRrRp0qhE9W35-hIeNvAkfX1mMb-K_PzeP47ZPLwBPrCViASlcHcgYiAKKSh3X2ZH4BLbp-YZRicg9Su8Z1uBd3He5ZXnYx'
     headers = {'Authorization': 'Bearer %s' % api_key}
     url='https://api.yelp.com/v3/businesses/search'
-    params = {'term': 'restaurants', 'location':'california', 'offset':0, 'limit':50}
+    params = {'term': 'restaurants', 'location':'manhattan', 'offset':0, 'limit':50}
     
     final_businesses=[]
-    while params['offset']<951:
+    offset = params['offset']
+    while params['offset']<offset+951:
         req=requests.get(url, params=params, headers=headers)
         print('Total restaurants: {}'.format(params['offset']+50))
         params['offset']+=50
@@ -20,7 +21,7 @@ def main():
         if len(businesses)<50:
             break
     print('number of restaurants are {}'.format(len(final_businesses)))
-    with open('../data/ca_restaurants.json', 'w') as f:
+    with open('../data/manhattan_restaurants1.json', 'w') as f:
         f.write(json.dumps({'businesses': final_businesses}, indent=4, separators=(',',':')))
 
 if __name__ == '__main__':
