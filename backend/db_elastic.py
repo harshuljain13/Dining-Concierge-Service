eee54eb953645bdb06ae0e75561019b80d6d2eb3
ak7907 <ak7907@nyu.edu>
2020-03-04 21:44:53 -0500

    elastic

--- backend/db_elastic.py
+++ backend/db_elastic.py
@@ -7,16 +7,9 @@ from boto3.dynamodb.conditions import Key, Attr
 import boto.dynamodb2
 from boto.dynamodb2.table import Table
 from time import sleep
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
 
-# Helper class to convert a DynamoDB item to JSON.
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1', aws_access_key_id='AKIATMX4AJ2YKGC2Z4MT',
          aws_secret_access_key= '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')
@@ -39,6 +32,38 @@ for each_entry in data:
     final_data.append([each_entry["name"],each_entry["zipcode"]])
 
 print (cuisine)
+
+
+client = boto3.client("es")
+
+
+
+
+# The endpoint of ES
+host = "search-dining-concierge-ogeltqbendc4e6kdwwrhhqhjzq.us-east-1.es.amazonaws.com"
+
+
+
+service = "es"
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service)
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+count = 0
+
+es.index(index="restaurants", doc_type="restaurant", id=restaurantId, body=document)
+
+# Verify that the document was successfully indexed
+check = es.get(index="restaurants", doc_type="restaurant", id=restaurantId)
+if check["found"]:
+    print("Index %s succeeded" % restaurantId)
 
 
 
